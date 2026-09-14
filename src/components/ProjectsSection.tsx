6524f4f1c99e1e4e61a6f1dbf24bed1dbd1dfba6
chipukizii <koechgilton@gmail.com>
2026-09-14 09:59:48 +0300

    fix the fashion store link

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -21,11 +21,11 @@ const projects = [
     color: 'from-green-500 to-emerald-700'
   },
   {
-    title: 'Daycare Services',
+    title: 'Fashion Store services ',
     description:
-      'A comprehensive platform for daycare service management, scheduling, and booking for parents.',
+      'A comprehensive platform for fashion service management, scheduling, and booking for kenyans.',
     tags: ['HTML/CSS/JS', 'Django', 'Full Stack'],
-    link: 'https://daycare-services-c7n5.onrender.com/',
+    link: 'lukustore.onrender.com/',
     image: '/projects/daycare.png',
     color: 'from-pink-500 to-rose-700'
   },
